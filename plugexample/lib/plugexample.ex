bd78bbc81e311fc3e59b0e5f811ba9d8105c68ce
Luiz Cezer <lccezinha@gmail.com>
2018-06-26 20:57:35 -0300

    Minimal Plug example

---
+++ plugexample/lib/plugexample.ex
@@ -0,0 +1,15 @@
+defmodule Plugexample do
+  use Application
+
+  require Logger
+
+  def start(_type, _args) do
+    children = [
+      Plug.Adapters.Cowboy.child_spec(:http, Example.HelloWorldPlug, [], port: 8080)
+    ]
+
+    Logger.info("Started app")
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
---
+++ plugexample/test/plugexample_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugexampleTest do
+  use ExUnit.Case
+  doctest Plugexample
+
+  test "greets the world" do
+    assert 1 == 1
+  end
+end
