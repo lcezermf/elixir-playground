8415c03d1edc9b870d5e4725f5469977224f2800
Luiz Cezer <lccezinha@gmail.com>
2018-06-28 21:38:18 -0300

    Update anagram

--- anagram/lib/anagram.ex
+++ anagram/lib/anagram.ex
@@ -1,23 +1,23 @@
 defmodule Anagram do
   def validate(word_a, word_b) do
     cond do
-      !is_binary(word_a) || !is_binary(word_b) || invalid_string(word_a) || invalid_string(word_b) ->
+      invalid_string(word_a) || invalid_string(word_b) ->
         raise RuntimeError, message: "must fill arguments with 2 valid strings"
       true ->
         anagrams?(word_a, word_b)
     end
   end
 
-  defp invalid_string(word) do
+  def invalid_string(word) do
+    !is_binary(word) || blank_string(word)
+  end
+
+  defp blank_string(word) do
     String.length(String.trim(word)) == 0
   end
 
   defp anagrams?(word_a, word_b) do
-    if sort_string(word_a) == sort_string(word_b) do
-      "Anagram!"
-    else
-      "Not an Anagram!"
-    end
+    if sort_string(word_a) == sort_string(word_b), do: "Anagram!", else: "Not an Anagram!"
   end
 
   defp sort_string(word) do
