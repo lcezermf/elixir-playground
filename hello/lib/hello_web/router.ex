2f466831912f8ca54fbaa6185c9935df0c49b037
Luiz Cezer <lccezinha@gmail.com>
2018-07-03 21:38:51 -0300

    Play with routes

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -13,12 +13,24 @@ defmodule HelloWeb.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/admin", HelloWeb.Admin, as: :admin do
+    pipe_through :browser
+
+    resources "/images", ImageController
+    resources "/reviews", ReviewController
+    resources "/users", UserController
+  end
+
   scope "/", HelloWeb do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+    resources "/reviews", HelloWeb.ReviewController
+    resources "/users", UserController do
+      resources "/posts", PostController
+    end
   end
 
   # Other scopes may use custom stacks.
