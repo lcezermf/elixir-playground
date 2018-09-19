ce71186345910e5281bafcfba996e0e9778af960
Luiz Cezer <lccezinha@gmail.com>
2018-09-18 22:16:02 -0300

    Add test cases

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,21 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck must return a list of strings" do
+    deck = Cards.create_deck
+
+    assert deck |> is_list
+  end
+
+  test "create_deck must return a list with 20 cards" do
+    deck = Cards.create_deck
+
+    assert length(deck) == 20
+  end
+
+  test "shuffle a deck must randomizes it" do
+    deck = Cards.create_deck
+
+    assert deck != Cards.shuffle(deck)
   end
 end
