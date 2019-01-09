7e7b154357dfc845c3965025c14484b771f97019
Luiz Cezer <lccezinha@gmail.com>
2019-01-09 21:27:48 -0200

    Set a setup_all for tests

--- todoer/test/todoer_test.exs
+++ todoer/test/todoer_test.exs
@@ -2,36 +2,36 @@ defmodule TodoerTest do
   use ExUnit.Case
   doctest Todoer
 
+  setup_all do
+    {:ok, todo_list: %{}}
+  end
+
   describe ".new" do
-    test "must return new Todoer instance" do
-      assert Todoer.new() == %{}
+    test "must return new Todoer instance", state do
+      assert Todoer.new() == state[:todo_list]
     end
   end
 
   describe ".add_entry/3" do
-    test "must add a new entry" do
-      todo_list = Todoer.new()
+    test "must add a new entry", state do
       details = %{date: {2019, 18, 1}, title: "Go to Dentist!"}
 
-      assert %{{2019, 18, 1} => ["Go to Dentist!"]} == Todoer.add_entry(todo_list, details)
+      assert %{{2019, 18, 1} => ["Go to Dentist!"]} ==
+               Todoer.add_entry(state[:todo_list], details)
     end
   end
 
   describe ".entries/2" do
-    test "must return entries for a given date" do
-      todo_list = Todoer.new()
+    test "must return entries for a given date", state do
       details = %{date: {2019, 18, 1}, title: "Go to Dentist!"}
 
-      new_todo = Todoer.add_entry(todo_list, details)
+      new_todo = Todoer.add_entry(state[:todo_list], details)
 
       assert ["Go to Dentist!"] == Todoer.entries(new_todo, details.date)
     end
 
-    test "return empty list for a date with no todo" do
-      todo_list = Todoer.new()
-      date = {2019, 18, 1}
-
-      assert [] == Todoer.entries(todo_list, date)
+    test "return empty list for a date with no todo", state do
+      assert [] == Todoer.entries(state[:todo_list], {2019, 18, 1})
     end
   end
 end
