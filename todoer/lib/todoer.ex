178d9d61b34307304cf34213cf37110b778f84fd
Luiz Cezer <lccezinha@gmail.com>
2019-01-10 16:59:56 -0200

    Add update_entry function

--- todoer/lib/todoer.ex
+++ todoer/lib/todoer.ex
@@ -51,4 +51,38 @@ defmodule Todoer do
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
     |> Enum.map(fn {_, entry} -> entry end)
   end
+
+  @doc """
+  Update an entry information based on the ID
+
+  When given ID is invalid, must return the current list without any change.
+
+  ## Examples
+
+    iex> todo_list = Todoer.new() |> Todoer.add_entry(%{date: {2019, 18, 1}, title: "Go to Dentist!"}) |> Todoer.add_entry(%{date: {2019, 18, 2}, title: "Go to Work!"})
+    iex> Todoer.update_entry(todo_list, 1, &Map.put(&1, :title, "New Title"))
+    %Todoer{
+              auto_id: 3,
+              entries: %{
+                1 => %{date: {2019, 18, 1}, id: 1, title: "New Title"},
+                2 => %{date: {2019, 18, 2}, id: 2, title: "Go to Work!"}
+              }
+            }
+
+  """
+  # def update_entry(todo_list, entry_id, %{} = entry_new_data) do
+  #   update_entry(todo_list, entry_id, fn _ -> entry_new_data end)
+  # end
+
+  def update_entry(%Todoer{entries: entries} = todo_list, entry_id, updater_fun) do
+    case entries[entry_id] do
+      nil ->
+        todo_list
+
+      old_entry ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(entries, new_entry.id, new_entry)
+        %Todoer{todo_list | entries: new_entries}
+    end
+  end
 end
--- todoer/test/todoer_test.exs
+++ todoer/test/todoer_test.exs
@@ -45,4 +45,33 @@ defmodule TodoerTest do
       assert [] == Todoer.entries(state[:todo_list], {2019, 18, 1})
     end
   end
+
+  describe ".update_entry/2" do
+    test "given a valid ID must update the entry", state do
+      old_date = {2019, 18, 1}
+      todo_list = Todoer.add_entry(state[:todo_list], %{date: old_date, title: "Go to Dentist!"})
+
+      new_date = {2020, 2, 10}
+      updated_entries = Todoer.update_entry(todo_list, 1, &Map.put(&1, :date, new_date))
+      # updated_entries = Todoer.update_entry(todo_list, 1, %{date: new_date})
+      {_, updated_entry} = Enum.at(updated_entries.entries, 0)
+
+      assert length(Map.keys(todo_list.entries)) == 1
+
+      assert updated_entry.date == new_date
+      refute updated_entry.date == old_date
+    end
+
+    test "given an INVALID ID must do nothing", state do
+      todo_list =
+        Todoer.add_entry(state[:todo_list], %{date: {2019, 18, 1}, title: "Go to Dentist!"})
+
+      updated_entries = Todoer.update_entry(todo_list, 10, &Map.put(&1, :title, "Another Title"))
+
+      assert todo_list.entries == updated_entries.entries
+
+      {_, entry} = Enum.at(todo_list.entries, 0)
+      assert entry.title == "Go to Dentist!"
+    end
+  end
 end
