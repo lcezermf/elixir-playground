530b7460e4b65f9f91885b5aa16a9c31743ccbf2
Luiz Cezer <lccezinha@gmail.com>
2019-01-23 17:20:08 -0200

    Simple server

--- concurrency-2.exs
+++ concurrency-2.exs
@@ -21,6 +21,25 @@ receive do
   after 5000 -> IO.puts("Message not received")
 end
 
-Message not received
+"Message not received"
 
+run_query = fn(query_def) ->
+  :timer.sleep(2000)
+  "#{query_def} result"
+end
 
+async_query = fn(query_def) ->
+  caller = self
+
+  spawn(fn ->
+    send(caller, {:query_result, run_query.(query_def)})
+  end)
+end
+
+get_result = fn ->
+  receive do
+    {:query_result, result} -> result
+  end
+end
+
+1..5 |> Enum.map(&async_query.("query #{&1}")) |> Enum.map(fn(_) ->  get_result.() end)
