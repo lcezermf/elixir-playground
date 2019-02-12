a9b17ad09ec38239b17cb5fc40da828fe68976ca
Luiz Cezer <lccezinha@gmail.com>
2019-02-12 08:35:13 -0200

    Enable to use async requests

--- gen-server-process.exs
+++ gen-server-process.exs
@@ -1,27 +1,40 @@
 defmodule GenericServerProcess do
   def start(callback_module) do
     spawn(fn ->
-      initial_state = callback_module.init
+      initial_state = callback_module.init()
       loop(callback_module, initial_state)
     end)
   end
 
   def loop(callback_module, current_state) do
     receive do
-      {request, caller} ->
+      {:call, request, caller} ->
         {response, new_state} = callback_module.handle_call(request, current_state)
         send(caller, {:response, response})
+
+        loop(callback_module, new_state)
+
+      {:cast, request} ->
+        new_state =
+          callback_module.handle_cast(
+            request,
+            current_state
+          )
 
         loop(callback_module, new_state)
     end
   end
 
   def call(pid, request) do
-    send(pid, {request, self})
+    send(pid, {:call, request, self})
 
     receive do
       {:response, response} -> response
     end
+  end
+
+  def cast(pid, request) do
+    send(pid, {:cast, request})
   end
 end
 
@@ -31,7 +44,7 @@ defmodule KeyValueStore do
   end
 
   def put(pid, key, value) do
-    GenericServerProcess.call(pid, {:put, key, value})
+    GenericServerProcess.cast(pid, {:put, key, value})
   end
 
   def get(pid, key) do
@@ -48,5 +61,9 @@ defmodule KeyValueStore do
 
   def handle_call({:get, key}, state) do
     {Map.get(state, key), state}
+  end
+
+  def handle_cast({:put, key, value}, state) do
+    Map.put(state, key, value)
   end
 end
