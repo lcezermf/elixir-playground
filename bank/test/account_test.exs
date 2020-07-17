f2e182107232f33e7ed6d7163e73b684b6b591b5
Luiz Cezer <lccezinha@gmail.com>
2020-07-16 23:30:03 -0300

    Add Setup to test

--- bank/test/account_test.exs
+++ bank/test/account_test.exs
@@ -1,33 +1,30 @@
 defmodule AccountTest do
   use ExUnit.Case
 
-  test "initial balance eq 0" do
+  setup do
     {:ok, pid} = Account.start_link()
 
+    %{pid: pid}
+  end
+
+  test "initial balance eq 0", %{pid: pid} do
     assert 0 == Account.get_balance(pid)
   end
 
-  test "depositing money must change the balance by 10" do
-    {:ok, pid} = Account.start_link()
-
+  test "depositing money must change the balance by 10", %{pid: pid} do
     Account.deposit(pid, 10)
 
     assert 10 == Account.get_balance(pid)
   end
 
-  test "withdraw money reduces balance by 5" do
-    {:ok, pid} = Account.start_link()
-
+  test "withdraw money reduces balance by 5", %{pid: pid} do
     Account.withdraw(pid, 5)
-
     assert -5 == Account.get_balance(pid)
 
     Account.deposit(pid, 10)
-
     assert 5 == Account.get_balance(pid)
 
     Account.withdraw(pid, 5)
-
     assert 0 == Account.get_balance(pid)
   end
 end
