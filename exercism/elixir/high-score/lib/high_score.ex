b155fce2caeb7a00385281fce0edda024a744b72
Luiz Cezer <lccezinha@gmail.com>
2021-09-20 23:16:59 -0300

    Add exercism tracks

--- exercism/elixir/high-score/lib/high_score.ex
+++ exercism/elixir/high-score/lib/high_score.ex
@@ -1,9 +1,11 @@
 defmodule HighScore do
+  @score 0
+
   def new() do
     %{}
   end
 
-  def add_player(scores, name, score \\ 0) do
+  def add_player(scores, name, score \\ @score) do
     Map.put(scores, name, score)
   end
 
@@ -12,7 +14,7 @@ defmodule HighScore do
   end
 
   def reset_score(scores, name) do
-    Map.put(scores, name, 0)
+    Map.put(scores, name, @score)
   end
 
   def update_score(scores, name, score) do
