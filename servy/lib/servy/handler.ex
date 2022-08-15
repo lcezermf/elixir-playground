f807351aca9f023fa5e8484451b21efa06067262
Luiz Cezer <lccezinha@gmail.com>
2022-08-15 18:57:14 -0300

    Pattern match all routes with maps

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -2,8 +2,10 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse()
-    |> log()
+    |> rewrite_path()
     |> route()
+    |> log()
+    |> track()
     |> format_response()
   end
 
@@ -19,29 +21,39 @@ defmodule Servy.Handler do
 
   def log(request), do: IO.inspect(request)
 
-  def route(request) do
-    route(request, request.method, request.path)
+  def rewrite_path(%{path: "/wildlife"} = request) do
+    %{request | path: "/wildthings"}
   end
 
-  def route(request, "GET", "/wildthings") do
+  def rewrite_path(request), do: request
+
+  def route(%{method: "GET", path: "/wildthings"} = request) do
     %{request | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(request, "GET", "/bears") do
+  def route(%{method: "GET", path: "/bears"} = request) do
     %{request | status: 200, resp_body: "Bear #1, Bear #2, Bear #3"}
   end
 
-  def route(request, "GET", "/bears/" <> id) do
+  def route(%{method: "GET", path: "/bears/" <> id} = request) do
     %{request | status: 200, resp_body: "Bear ID #{id}"}
   end
 
-  def route(request, "DELETE", "/bears/" <> id) do
-    %{request | status: 200, resp_body: "Bear ID #{id} deleted"}
+  def route(%{method: "DELETE", path: "/bears/" <> _id} = request) do
+    %{ request | status: 403, resp_body: "Deleting a bear is forbidden!"}
   end
 
-  def route(request, _method, path) do
+  def route(%{path: path} = request) do
     %{request | status: 404, resp_body: "No #{path} here!"}
   end
+
+  def track(%{status: 404, path: path} = request) do
+    IO.puts("Warning: #{path} is not here")
+
+    request
+  end
+
+  def track(request), do: request
 
   def format_response(request) do
     """
@@ -99,6 +111,16 @@ IO.puts(Servy.Handler.handle(request))
 
 request = """
 DELETE /bears/25 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+IO.puts(Servy.Handler.handle(request))
+
+request = """
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
