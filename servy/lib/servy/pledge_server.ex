11684a2ac535068e7f9d58710ba9c0a4f9ebcccc
Luiz Cezer <lccezinha@gmail.com>
2022-09-08 16:12:02 -0300

    Add route to pledge

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -1,24 +1,13 @@
 defmodule Servy.PledgeServer do
   @name :pledge_server
 
+  # Client/API functions
+
   def start do
     IO.puts("Starting...")
     pid = spawn(__MODULE__, :listen_loop, [[]])
     Process.register(pid, @name)
     pid
-  end
-
-  def listen_loop(state) do
-    receive do
-      {sender, :create_pledge, name, amount} ->
-        {:ok, id} = send_pledge_to_service(name, amount)
-        new_state = [{name, amount} | Enum.take(state, 2)]
-        send(sender, {:response, id})
-        listen_loop(new_state)
-      {sender, :recent_pledges} ->
-        send(sender, {:response, state})
-        listen_loop(state)
-    end
   end
 
   def create_pledge(name, amount) do
@@ -36,10 +25,21 @@ defmodule Servy.PledgeServer do
       {:response, pleges} -> pleges
     end
   end
-  #
-  # def recent_pledges do
-  #   []
-  # end
+
+  # Server functions
+
+  def listen_loop(state) do
+    receive do
+      {sender, :create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+        new_state = [{name, amount} | Enum.take(state, 2)]
+        send(sender, {:response, id})
+        listen_loop(new_state)
+      {sender, :recent_pledges} ->
+        send(sender, {:response, state})
+        listen_loop(state)
+    end
+  end
 
   def send_pledge_to_service(_name, _amount) do
     {:ok, "pledge-#{:rand.uniform(1_000)}"}
