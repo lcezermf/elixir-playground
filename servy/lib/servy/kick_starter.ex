38e611f5e39963026a9a60c0af93ac1b6292a9ea
Luiz Cezer <lccezinha@gmail.com>
2022-09-19 18:42:14 -0300

    Return process PID

--- servy/lib/servy/kick_starter.ex
+++ servy/lib/servy/kick_starter.ex
@@ -6,21 +6,32 @@ defmodule Servy.KickStarter do
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
 
+  def get_server do
+    GenServer.call(__MODULE__, :get_server)
+  end
+
   def init(:ok) do
     Process.flag(:trap_exit, true)
-    IO.puts("Starting http server")
-    server_pid = spawn(Servy.HttpServer, :start, [4000])
-    Process.link(server_pid)
-    Process.register(server_pid, :http_server)
+    server_pid = start_server()
     {:ok, server_pid}
+  end
+
+  # Server
+
+  def handle_call(:get_server, _from, state) do
+    {:reply, Process.whereis(:http_server), state}
   end
 
   def handle_info({:EXIT, _pid, reason}, _state) do
     IO.puts("Exiting: #{reason}")
-    IO.puts("Starting http server")
-    server_pid = spawn(Servy.HttpServer, :start, [4000])
-    Process.link(server_pid)
-    Process.register(server_pid, :http_server)
+    server_pid = start_server()
     {:noreply, server_pid}
+  end
+
+  defp start_server do
+    IO.puts("Starting http server")
+    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
+    Process.register(server_pid, :http_server)
+    server_pid
   end
 end
